9494ebf2c4680b09fcc012e60cc1b277eb98adf9
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-11-13 21:46:27 +0530

    category page is added

--- src/components/CategoryCards.jsx
+++ src/components/CategoryCards.jsx
@@ -4,7 +4,7 @@ import economyIcon from "../assets/Icons/economyIcon.svg";
 import technologyIcon from "../assets/Icons/technologyIcon.svg";
 
 
-function CategoryCards() {
+function CategoryCards({isCategoryPage = false}) {
     const categoryData = [
         {
             imageUrl: businessIcon,
@@ -30,17 +30,27 @@ function CategoryCards() {
 
     return (
         <>
-            <div className="flex w-full gap-6">
-            {categoryData.map(({ imageUrl, categoryName, text }, index) => (
-                <div key={index} className="flex flex-col gap-3 p-8 border-2 border-gray-200 w-[25%] hover:bg-Yellow cursor-pointer group">
-                    <div className="flex items-center justify-center w-12 h-12 bg-yellow-100 group-hover:bg-white rounded-xl ">
-                    {imageUrl && <img className="" src={imageUrl} alt={categoryName} />}
+            {isCategoryPage && (
+                categoryData.map(({ imageUrl, categoryName }, index) => (
+                    <div key={index} className="flex items-center gap-3 p-6 border-[1px] border-gray-200 cursor-pointer hover:bg-Yellow group">
+                            <div className="flex items-center justify-center w-8 h-8 bg-yellow-200 rounded-lg group-hover:bg-white">
+                            {imageUrl && <img className="" src={imageUrl} alt={categoryName} />}
+                            </div>
+                            <h2 className="text-2xl font-semibold">{categoryName}</h2>
                     </div>
-                    <h2 className="text-2xl font-bold">{categoryName}</h2>
-                    <p>{text}</p>
-                </div>
-            ))}
-            </div>
+                ))
+            )}
+            {!isCategoryPage && (
+                categoryData.map(({ imageUrl, categoryName, text }, index) => (
+                    <div key={index} className="flex flex-col gap-3 p-8 border-2 border-gray-200 w-[25%] hover:bg-Yellow cursor-pointer group">
+                        <div className="flex items-center justify-center w-12 h-12 bg-yellow-100 group-hover:bg-white rounded-xl ">
+                        {imageUrl && <img className="" src={imageUrl} alt={categoryName} />}
+                        </div>
+                        <h2 className="text-2xl font-bold">{categoryName}</h2>
+                        <p>{text}</p>
+                    </div>
+                ))
+            )}
         </>
     )
 }
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,7 +1,80 @@
+import post_image from "../assets/images/two-women-in-front-of-dry-erase-board-1181533-1.svg";
+import CategoryCards from "../components/CategoryCards";
+
+
 function Category() {
   return (
     <>
-      <h1>Category</h1>
+      {/* <h1>Category</h1> */}
+      {/* section 1 */}
+      <section className="flex justify-center bg-pink-100">
+        <div className="flex flex-col items-center w-[50%] my-20">
+        <h1 className="mb-4 text-5xl font-bold">Business</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, doloremque.</p>
+        <p className="mb-4">Lorem ipsum dolor sit amet consectetur.</p>
+        <p className="font-semibold">BLOG {">"} BUSINESS</p>
+        </div>
+      </section>
+
+      {/* section 2 */}
+      <section className="flex px-16 mt-24">
+        <div className="w-[70%] flex flex-col gap-12">
+          <div className="flex items-center gap-6 ">
+            <img className="w-[42%]" src={post_image} alt="" />
+            <div className="pr-5">
+              <p className="mb-3 font-semibold">BUSINESS</p>
+              <p className="mb-4 text-3xl font-semibold ">Top 6 free website mockup tools 2022</p>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, illo ad! Molestiae nemo exercitationem enim optio repudiandae porro cumque quas.</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-6 ">
+            <img className="w-[42%]" src={post_image} alt="" />
+            <div className="pr-5">
+              <p className="mb-3 font-semibold">BUSINESS</p>
+              <p className="mb-4 text-3xl font-semibold ">Step-by-step guide to choosing great font pairs</p>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, illo ad! Molestiae nemo exercitationem enim optio repudiandae porro cumque quas.</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-6 ">
+            <img className="w-[42%]" src={post_image} alt="" />
+            <div className="pr-5">
+              <p className="mb-3 font-semibold">BUSINESS</p>
+              <p className="mb-4 text-3xl font-semibold ">Ten free foogle fonts that you should use</p>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, illo ad! Molestiae nemo exercitationem enim optio repudiandae porro cumque quas.</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-6 ">
+            <img className="w-[42%]" src={post_image} alt="" />
+            <div className="pr-5">
+              <p className="mb-3 font-semibold">BUSINESS</p>
+              <p className="mb-4 text-3xl font-semibold ">What did I learn from doing 50+ design sprints?</p>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, illo ad! Molestiae nemo exercitationem enim optio repudiandae porro cumque quas.</p>
+            </div>
+          </div>
+        </div>
+        {/* categories and All tages */}
+        <div className="w-[30%] pl-16">
+          <h1 className="mb-12 text-3xl font-semibold">Categories</h1>
+          <div className="flex flex-col gap-4">
+            <CategoryCards isCategoryPage={true} />
+          </div>
+          <h1 className="my-12 text-3xl font-semibold">All Tags</h1>
+          <div className="flex flex-col gap-4">
+            <div className="flex w-full gap-4">
+              <button className="rounded-full border-[1px] border-gray-400 px-8 py-3 hover:bg-Yellow">Business</button>
+              <button className="rounded-full border-[1px] border-gray-400 w-full py-3 hover:bg-Yellow">Experience</button>
+            </div>
+            <div className="flex w-full gap-4">
+              <button className="rounded-full border-[1px] border-gray-400 px-8 py-3 hover:bg-Yellow">Screen</button>
+              <button className="rounded-full border-[1px] border-gray-400 w-full py-3 hover:bg-Yellow">Technology</button>
+            </div>
+            <div className="flex w-full gap-4">
+              <button className="rounded-full border-[1px] border-gray-400 w-full py-3 hover:bg-Yellow">Marketing</button>
+              <button className="rounded-full border-[1px] border-gray-400 px-8 py-3 hover:bg-Yellow">Life</button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
